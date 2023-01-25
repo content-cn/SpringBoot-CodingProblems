d6af237f2ce1e66f9d22733a4f57d0a3e4cfc91d
CodingNinjas <content@codingninjas.com>
2023-01-25 15:49:16 +0530

    Renamed cnkart to eventManagement

---
+++ eventManagement/src/main/java/com/cn/cnEvent/dal/EventDALImpl.java
@@ -0,0 +1,47 @@
+package com.cn.cnEvent.dal;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.cn.cnEvent.entity.Event;
+
+@Repository
+public class EventDALImpl implements EventDAL {
+
+	@Autowired
+	EntityManager entityManager;
+	
+	@Override
+	public Event getById(int id) {
+		Session session = entityManager.unwrap(Session.class);
+		Event event = session.get(Event.class, id);
+		return event;
+	}
+
+	@Override
+	public void save(Event event) {
+		Session session = entityManager.unwrap(Session.class);
+		session.save(event);
+	}
+
+	@Override
+	public void delete(int id) {
+		Session session = entityManager.unwrap(Session.class);
+		Event event = session.get(Event.class, id);
+		session.delete(event);
+		
+	}
+
+	@Override
+	public void update(Event updateEvent) {
+		Session session = entityManager.unwrap(Session.class);
+		Event currentEvent = session.get(Event.class, updateEvent.getId());
+		currentEvent.setName(updateEvent.getName());
+		session.update(currentEvent);
+		
+	}
+	
+}
---
+++ eventManagement/src/main/java/com/cn/cnEvent/service/EventService.java
@@ -0,0 +1,40 @@
+package com.cn.cnEvent.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cn.cnEvent.dal.EventDAL;
+import com.cn.cnEvent.entity.Event;
+
+@Service
+public class EventService {
+
+	@Autowired
+	EventDAL eventDAL;
+	
+	@Transactional
+	public Event getEventById(int id) {
+		return eventDAL.getById(id);
+	}
+
+	@Transactional
+	public void saveEvent(Event event) {
+		
+		eventDAL.save(event);
+	}
+
+	@Transactional
+	public void delete(int id) {
+		eventDAL.delete(id);
+		
+	}
+
+	@Transactional
+	public void update(Event updateEvent) {
+	
+		eventDAL.update(updateEvent);
+	}
+	
+}
